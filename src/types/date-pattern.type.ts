15a9e0dd16a77bb4d019d260281352722c689693
Gustavo Esteves <gustavoesteves@directon.com.br>
2026-03-14 12:24:30 -0300

    feat: tipagem data aceita e formatação data

---
+++ src/types/date-pattern.type.ts
@@ -0,0 +1,10 @@
+export type DateType = string | number | Date;
+export type PatternType =
+  | "dd/MM/yyyy"
+  | "dd/MM/yy"
+  | "dd/MM"
+  | "dd/MM/yyyy HH:mm"
+  | "dd/MM/yyyy HH:mm:ss"
+  | "HH:mm"
+  | "HH:mm:ss"
+  | "mm:ss";
