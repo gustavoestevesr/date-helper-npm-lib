6e8230ce8e68a6bd48d2e4e1cee1ca0ce4d89d1e
Gustavo Esteves <gustavoesteves@directon.com.br>
2026-03-14 16:15:00 -0300

    feat: novos types de datas, horas, dias da semana

--- src/types/date-pattern.type.ts
+++ src/types/date-pattern.type.ts
@@ -1,10 +1,11 @@
 export type DateType = string | number | Date;
 export type PatternType =
-  | "dd/MM/yyyy"
-  | "dd/MM/yy"
-  | "dd/MM"
-  | "dd/MM/yyyy HH:mm"
-  | "dd/MM/yyyy HH:mm:ss"
-  | "HH:mm"
-  | "HH:mm:ss"
-  | "mm:ss";
+  | DatePatterType
+  | TimePatternType
+  | DateTimePatterType
+  | DayWeekPatternType;
+export type DatePatterType = "dd/MM/yyyy" | "dd/MM/yy" | "dd/MM";
+export type DateTimePatterType = "dd/MM/yyyy HH:mm" | "dd/MM/yyyy HH:mm:ss";
+export type TimePatternType = "HH:mm" | "HH:mm:ss" | "mm:ss" | "mm" | "ss";
+export type DayWeekPatternType = "E..EEE" | "EEE" | "EEEE" | "EEEEE";
+export type DayWeekFormats = Record<DayWeekPatternType, string>;
